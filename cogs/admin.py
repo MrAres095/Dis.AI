188b5ac3ad6e2644411f3240780e7b7fe0ade670
jacobjude <jacob.jude03@gmail.com>
2023-04-05 12:14:52 -0400

    fixed admin

--- cogs/admin.py
+++ cogs/admin.py
@@ -16,7 +16,7 @@ class AdminCommands(commands.Cog):
         server = self.bot.get_guild(int(serverid))
         channels = server.channels
         for channelz in channels:
-            print(channelz.name)
+            print(f"Name: {channelz.name} id: {channelz.id}")
         channel = server.get_channel(int(channelid))
         embed = discord.Embed(title="[System Notice]", description = "Dis.AI has updated to v1.1. As a result of major backend changes, all data has been reset. Please use the following to set up a new chatbot: ```/createchatbot```\n\n**New feature:** chatbots now have web search abilities. Prefix your message with \"search\" to tell the chatbot to retrieve info from the web.\nFor example: ```search who the newest valorant agent is```Add or remove search prefixes with /settings\n\nThis will be the last notice of this kind.", color=discord.Colour.blue())
         await channel.send(embed=embed)
